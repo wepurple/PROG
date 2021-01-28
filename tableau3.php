0f5e579ecd21cf8dbf1e300412119966fbd890be
Kilian Moliere <eprosnip@hotmail.fr>
2021-01-28 15:53:48 +0100

    Commit 4

--- tableau3.php
+++ tableau3.php
@@ -1,5 +1,5 @@
 <?php
-$marque = array("volvo","BMW","Toyota", "Peugeot","Renault",);
+$marque = array("volvo","BMW","Toyota", "Peugeot","Renault", "Porsche", "Lamborghini");
 $i=0;
 foreach ($elem as $marque) {
 	echo "Vente de : " . $elem . "<br/>";
